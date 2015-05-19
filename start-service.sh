43fed8cbd442f7c84793469058a182660ceb1719
Gary Rowe <gary.rowe@metoffice.gov.uk>
2015-05-19 13:17:48 +0100

    Add start-service script

---
+++ start-service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Starting Error Reporting Service with PID: $$;
+echo Usage:
+echo 1. At the password prompt, enter the password for the secret key ring
+echo 2. Send the service to background with CTRL+Z
+echo 3. Restart the stopped job in the background by typing 'bg'
+echo 4. Exit from shell
+echo 5. Verify service is still running by checking
+echo ..   TEST: http://localhost:7070/error-reporting/public-key   or
+echo ..   LIVE: https://multibit.org/error-reporting/public-key
+echo ..
+echo TIP: You can find this process again by typing 'ps -A | grep brit'
+java -cp "bcprov-jdk16-1.46.jar:target/error-reporting-service-develop-SNAPSHOT.jar" org.multibit.hd.error_reporting.ErrorReportingService server config.yml
