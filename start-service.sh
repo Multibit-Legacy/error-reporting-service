f094bc639cb560b5511fb9ee87c62b93ff24c938
Gary Rowe <gary.rowe@metoffice.gov.uk>
2015-05-19 14:27:58 +0100

    Tidy up start script

--- start-service.sh
+++ start-service.sh
@@ -6,8 +6,9 @@ echo 2. Send the service to background with CTRL+Z
 echo 3. Restart the stopped job in the background by typing 'bg'
 echo 4. Exit from shell
 echo 5. Verify service is still running by checking
-echo ..   TEST: http://localhost:7070/error-reporting/public-key   or
+echo ..   TEST: http://localhost:9191/error-reporting/public-key   or
 echo ..   LIVE: https://multibit.org/error-reporting/public-key
 echo ..
 echo TIP: You can find this process again by typing 'ps -A | grep error-reporting'
-java -cp "bcprov-jdk16-1.46.jar:target/error-reporting-service-develop-SNAPSHOT.jar" org.multibit.hd.error_reporting.ErrorReportingService server config.yml
+echo TIP: If 'NoClassDefFoundError: ... BouncyCastleProvider' copy bcprov-jdk16-1.46.jar to project root
+java -cp "bcprov-jdk16-1.46.jar" -jar target/error-reporting-service-develop-SNAPSHOT.jar server config.yml
